3ad5c914a6d5f406c43dd9546ee830864eceb092
Harlan Haskins <harlan@valleyrocket.com>
2012-09-30 16:50:43 -0400

    Added history functionality.
    Answers and previous expressions are saved and available for use. When
    a user presses an operator and the box is empty, it automatically
    substitutes 'Ans.'

--- js/main.js
+++ js/main.js
@@ -1,25 +1,58 @@
+var box = document.getElementById('expressionbox');
+var lastEvaluatedExpression = '';
+
 function addTextToExpressionBox(text) {
-	var box = document.getElementById('expressionbox');
-	box.value += text;
+	"use strict";
+	//Check to see if the text being added is an operator. If it is, and the box is empty, the program assumes it's trying to
+	//work with the answer.
+	if (window.box.value === '' && (text === "+" || text === "-" || text === "*" || text === "/")) {
+		window.box.value += 'Ans';
+	}
+	//Add the text to the bar. The text is defined in index.html as the value passed by the buttons in the onclick() method.
+	window.box.value += text;
 }
 
-function addSolutionToHistoryBox() {
-	document.getElementById('historybox').value = solution();
-	clearExpressionBox();
-}
-
-function solution() {
-	var box = document.getElementById('expressionbox');
-	var expression = Parser.evaluate(box.value);
-	return expression;
+function equalsButtonPressed() {
+	if (window.box.value === '' && window.lastEvaluatedExpression != '') {
+		window.box.value = window.lastEvaluatedExpression;
+	}
+	addSolutionToHistoryBox();
 }
 
 function clearExpressionBox() {
+	"use strict";
+	//Set the ExpressionBox to an empty string.
 	document.getElementById('expressionbox').value = '';
 }
 
+function solution() {
+	//This method is not called by a button, just inside addSolutionToHistoryBox();
+	"use strict";
+	//Parse the expression that's in the Expression window.box. Also, since the parser allows for variable substitution, set the variable 'Ans'
+	//to the value of the history box.
+	var expression = Parser.evaluate(document.getElementById('expressionbox').value, {"Ans" : document.getElementById('historybox').value});
+	return expression;
+}
+
+function addSolutionToHistoryBox() {
+	"use strict";
+	//Get the solution. This method is called directly by the equals button.
+	var solutionString = solution();
+	//Set the history box and Answer button values to the solution.
+	document.getElementById('historybox').value = solutionString;
+	document.getElementById('ansbutton').value = 'ans ' + solutionString;
+	//Save the expression to the lastEvaluatedExpression var.
+	lastEvaluatedExpression = document.getElementById('expressionbox').value;
+	//Clear the expression box.
+	clearExpressionBox();
+}
+
 function backspaceExpressionBox() {
+	"use strict";
+	//Get the expressionbox's value.
 	var expressionString = document.getElementById('expressionbox').value;
+	//Return the same string, minus the last letter.
 	var newString = expressionString.substring(0, (expressionString.length - 1));
+	//Set the expressionbox to the new string. 
 	document.getElementById('expressionbox').value = newString;
 }
