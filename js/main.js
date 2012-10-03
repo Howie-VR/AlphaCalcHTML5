2bd82870215ae272e75efad1e9550792c7557c56
Harlan Haskins <harlan@valleyrocket.com>
2012-10-03 16:28:55 -0400

    Changed Ans to something more useful

--- js/main.js
+++ js/main.js
@@ -4,13 +4,14 @@ var parser = new Parser();
 function addTextToExpressionBox(text) {
 	"use strict";
 	var box = document.getElementById('expressionbox');
-	//Check to see if the text being added is an operator. If it is, and the box is empty, the program assumes it's trying to
+	//Check to see if the text being added is an operator (or @, which is our standin for '(Ans)'). If it is, and the box is empty, the program assumes it's trying to
 	//work with the answer.
-	if (box.value === '' && (text === "+" || text === "-" || text === "*" || text === "/")) {
-		box.value += 'Ans';
+	if (box.value === '' && (text === "+" /*|| text === "-" */ || text === "*" || text === "@" || text === "/" || text === "^")) {
+		box.value += '(Ans)';
 	}
 	//Add the text to the bar. The text is defined in index.html as the value passed by the buttons in the onclick() method.
-	box.value += text;
+	if (text != '@')
+		box.value += text;
 }
 
 function equalsButtonPressed() {
@@ -38,9 +39,16 @@ function solution() {
 	//Parse the expression that's in the Expression box. Also, since the parser allows for variable substitution, set the variable 'Ans'
 	//to the value of the history box.
 	var expression = document.getElementById('expressionbox').value;
+<<<<<<< HEAD
 	//expression = expression.replace('Ans', document.getElementById('historybox').value);
 	var rootExpressionNode = parser.parse(document.getElementById('expressionbox').value);
 	var answer = rootExpressionNode.evaluateExpression();
+=======
+	//Replace 'Ans' in the box with the value in the history box.
+	expression = expression.replace('Ans', document.getElementById('historybox').value);
+	//Parse expression using parser.js.
+	var answer = Parser.evaluate(expression);
+>>>>>>> Changed Ans to something more useful
 	return answer;
 }
 
@@ -48,11 +56,11 @@ function addSolutionToHistoryBox() {
 	"use strict";
 	//Get the solution.
 	var solutionString = solution();
+	//Save the expression to the lastEvaluatedExpression var.
+	lastEvaluatedExpression = document.getElementById('expressionbox').value;
 	//Set the history box and Answer button values to the solution.
 	document.getElementById('historybox').value = solutionString;
 	document.getElementById('previousanswer').innerText = solutionString;
-	//Save the expression to the lastEvaluatedExpression var.
-	lastEvaluatedExpression = document.getElementById('expressionbox').value;
 	//Clear the expression box.
 	clearExpressionBox();
 }
